9732f2ac038049babe113e5081c8e49cd53d1c38
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 02:52:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
     thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
+  return thankYouCards;
 }
